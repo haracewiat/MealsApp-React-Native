397948f50e8fbd1b1a9d0d617424d81ec899ea89
Beata Haracewiat <b.haracewiat@gmail.com>
2020-01-11 18:17:31 +0100

    Added base navigation
    The user is able to navigate between screens Categories, Category Meal and Meal Details.

---
+++ navigation/MealsNavigator.js
@@ -0,0 +1,14 @@
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+
+import CategoriesScreen from "../screens/CategoriesScreen";
+import CategoryMealsScreen from "../screens/CategoryMealsScreen";
+import MealDetailScreen from "../screens/MealDetailScreen";
+
+const MealsNavigator = createStackNavigator({
+  Categories: { screen: CategoriesScreen },
+  CategoryMeals: { screen: CategoryMealsScreen },
+  MealDetail: { screen: MealDetailScreen }
+});
+
+export default createAppContainer(MealsNavigator);
