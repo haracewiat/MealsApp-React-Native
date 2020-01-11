c80da724f304a6e42062f166ffd74a0d0eff8f1e
Beata Haracewiat <b.haracewiat@gmail.com>
2020-01-11 19:29:12 +0100

    Created a grid for Categories
    The categories are now displayed as colorful tiles.

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
+import { CATEGORIES } from "../data/dummy-data";
+
 const CategoryMealsScreen = props => {
+  const cid = props.navigation.getParam("categoryId");
+
+  const selectedCategory = CATEGORIES.find(category => category.id === cid);
   return (
     <View style={styles.screen}>
       <Text>The Category Meals Screen</Text>
+      <Text>{selectedCategory.title}</Text>
       <Button
         title="Details"
         onPress={() => {
@@ -13,6 +19,15 @@ const CategoryMealsScreen = props => {
       />
     </View>
   );
+};
+
+CategoryMealsScreen.navigationOptions = navigationData => {
+  const cid = navigationData.navigation.getParam("categoryId");
+  const selectedCategory = CATEGORIES.find(category => category.id === cid);
+
+  return {
+    headerTitle: selectedCategory.title
+  };
 };
 
 const styles = StyleSheet.create({
