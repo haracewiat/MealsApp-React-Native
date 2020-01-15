d2d97284b8558995f653045cee36c9f66fff4c9e
Beata Haracewiat <b.haracewiat@gmail.com>
2020-01-15 15:03:28 +0100

    Added an icon to the navigation bar
    A star icon is displayed next to the Meal title to allow the user to add a certain meal to their favourites in the future update.

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -34,7 +34,8 @@ const styles = StyleSheet.create({
     margin: 15,
     height: 150,
     borderRadius: 10,
-    overflow: "hidden"
+    overflow:
+      Platform.OS === "android" && Platform.Version >= 21 ? "hidden" : "visible"
   },
   container: {
     flex: 1,
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,7 +1,10 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import { MEALS } from "../data/dummy-data";
+import HeaderButton from "../components/HeaderButton";
+
 const MealDetailScreen = props => {
   const mealId = props.navigation.getParam("mealId");
 
@@ -18,7 +21,18 @@ MealDetailScreen.navigationOptions = navigationData => {
   const mealId = navigationData.navigation.getParam("mealId");
   const selectedMeal = MEALS.find(meal => meal.id === mealId);
   return {
-    headerTitle: selectedMeal.title
+    headerTitle: selectedMeal.title,
+    headerRight: () => {
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Favourite"
+          iconName="ios-star"
+          onPress={() => {
+            console.log("Favourite!");
+          }}
+        />
+      </HeaderButtons>;
+    }
   };
 };
 
